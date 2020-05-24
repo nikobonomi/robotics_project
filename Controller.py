993912f03066958b8862ca48bbc6fb08ccc39cdd
NikoBonomi <niko.bonomi@supsi.ch>
2020-05-24 18:52:43 +0200

    implementato go to goal nel controller

--- Controller.py
+++ Controller.py
@@ -5,6 +5,7 @@ import numpy as np
 from messaging.MessagingClient import MessagingClient
 from messaging.messages.TwoDPose import TwoDPose
 from messaging.messages.Velocity import Velocity
+from utils.ErrorComputing import ErrorComputing
 from utils.PID import PID
 from utils.RateKeeper import RateKeeper
 
@@ -16,22 +17,53 @@ class Controller:
         self.counter = 1
         self.rate = RateKeeper(freq)  # il rate è in hz
         self.pose = TwoDPose()
+        self.goal = TwoDPose()
+        self.goal.x = 200
+        self.goal.y = 100
 
-        self.pid = PID(freq, 1)
+        self.x_pid = PID(freq, 1)
+        self.theta_pid = PID(freq, 3, 0, 0.1)
         self.target_x = 100
+        self.done = False
 
     def handle_server_message(self, message):
         # mi assicuro che sia il messaggio giusto
         if message.is_type(TwoDPose):
             self.pose = message
 
-    def step(self):
-        distance_error = self.target_x - self.pose.x
+    def is_at_goal(self, goal, tolerance=10):
+        distance_error = ErrorComputing.euclidean_distance(self.pose, goal)
+        return distance_error < tolerance
 
-        x_vel = self.pid.compute(distance_error)
-        vel_msg = Velocity()
-        vel_msg.x = np.clip(x_vel, -20, 20)
-        self.messaging.publish_message(vel_msg)
+    def step_to_point(self, goal):
+        message = Velocity()
+
+        distance_error = ErrorComputing.euclidean_distance(self.pose, goal)
+        target_angle = ErrorComputing.steering_angle(self.pose, goal)
+        angle_error = ErrorComputing.angle_difference(target_angle, self.pose.theta)
+
+        x_vel = self.x_pid.compute(distance_error)
+        x_vel = np.clip(x_vel, 0, 20)
+        # Linear velocity in the x-axis.
+        message.x = x_vel
+
+        # Angular velocity in the z-axis.
+
+        z_vel = self.theta_pid.compute(angle_error)
+        z_vel = np.clip(z_vel, -0.4, 0.4)  # It's always a tourtle....
+
+        message.theta = z_vel
+
+        # Publishing our vel_msg
+        self.messaging.publish_message(message)
+
+    def step(self):
+        if not self.done:
+            self.step_to_point(self.goal)
+            self.done = self.is_at_goal(self.goal)
+        elif self.done:
+            # se è a posto allora ferma tutto
+            self.messaging.publish_message(Velocity())
 
         self.rate.wait_cycle()
 
--- utils/ErrorComputing.py
+++ utils/ErrorComputing.py
@@ -7,13 +7,13 @@ class ErrorComputing:
     # compute the distance beetwen 2 poses
     @staticmethod
     def euclidean_distance(actual_pose, goal_pose):
-        return sqrt(pow((goal_pose.position.x - actual_pose.position.x), 2) +
-                    pow((goal_pose.position.y - actual_pose.position.y), 2))
+        return sqrt(pow((goal_pose.x - actual_pose.x), 2) +
+                    pow((goal_pose.y - actual_pose.y), 2))
 
     # compute the theta from the current pose to the goal
     @staticmethod
     def steering_angle(actual_pose, goal_pose):
-        return atan2(goal_pose.position.y - actual_pose.position.y, goal_pose.position.x - actual_pose.position.x)
+        return atan2(goal_pose.y - actual_pose.y, goal_pose.x - actual_pose.x)
 
     # compute the difference beetwen 2 angles
     @staticmethod
