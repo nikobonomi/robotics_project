fb4391b7122977a157a3e216919cf907a8770cdb
Galgiani Matteo <galgiani@supsi.ch>
2020-05-28 00:17:09 +0200

    New SquareWall factory method instead of a new Obstacle class SquareWall.py

--- environment/PolygonWall.py
+++ environment/PolygonWall.py
@@ -6,3 +6,9 @@ class PolygonWall(Obstacle):
         vertex = vertexes
 
         Obstacle.__init__(self, identifier, vertex)
+
+    @classmethod
+    def square_wall(cls, identifier, x, y, width, height):
+        vertex = [x, y, x, y + height, x + width, y + height, x + width, y]
+
+        return cls(identifier, vertex)
--- environment/SquareWall.py
+++
@@ -1,8 +0,0 @@
-from environment.Obstacle import Obstacle
-
-
-class SquareWall(Obstacle):
-    def __init__(self, identifier, x, y, width, height):
-        vertex = [x, y, x, y + height, x + width, y + height, x + width, y]
-
-        Obstacle.__init__(self, identifier, vertex)
